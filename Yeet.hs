217d2aafb7061c28d2462fbedcd4f3e62366951a
Mareno Demmers <mhjdemmers@gmail.com>
2023-11-23 17:14:36 +0100

    yote

--- Yeet.hs
+++ Yeet.hs
@@ -1,10 +1,8 @@
-guessAge :: String -> Int -> String
-guessAge "Griselda" age
-    | age < 47 = "Too low!"
-    | age > 47 = "Too high!"
-    | otherwise = "Correct!"
-guessAge "Hansel" age
-    | age < 12 = "Too low!"
-    | age > 12 = "Too high!"
-    | otherwise = "Correct!"
-guessAge name age = "Wrong name!"
+readInt :: String -> Either String Int
+readInt "0" = Right 0
+readInt "1" = Right 1
+readInt s = Left ("Unsuppoerted string: " ++ s)
+
+iWantAString :: Either Int String -> String
+iWantAString (Right str)   = str
+iWantAString (Left number) = show number
