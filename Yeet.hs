f42b8309d24dd34e11236868bbcd9bbe667b8f56
Mareno Demmers <mhjdemmers@.gmail>
2024-02-11 21:00:23 +0100

    mare change

--- Yeet.hs
+++ Yeet.hs
@@ -50,3 +50,67 @@ doTwice f = f . f
 
 whatFollows :: Char -> Int -> String -> [String]
 whatFollows c k = map tail . filter ((==[c]) . take 1) . map (take (k+1)) . tails
+
+findSubstring :: String -> String -> String
+findSubstring chars = takeWhile (\x -> elem x chars)
+                      . dropWhile (\x -> not $ elem x chars)
+
+split :: Char -> String -> [String]
+split c [] = []
+split c xs = start : split c (drop 1 rest)
+  where start = takeWhile (/=c) xs
+        rest = dropWhile (/=c) xs
+
+myhead :: [Int] -> Int
+myhead [] = -1
+myhead (first:rest) = first
+
+mytail :: [Int] -> [Int]
+mytail [] = []
+mytail (first:rest) = rest
+
+sumFirstTwo :: [Integer] -> Integer
+-- this equation gets used for lists of length at least two
+sumFirstTwo (a:b:_) = a + b
+-- this equation gets used for all other lists (i.e. lists of length 0 or 1)
+sumFirstTwo _ = 0
+
+describeList :: [Int] -> String
+describeList [] = "an empty list"
+describeList [x] = "a list with one element"
+describeList [x,y] = "a list with two elements"
+describeList (x:y:z:xs) = "a list with at least three elements"
+
+startsWithZero :: [Integer] -> Bool
+startsWithZero (0:xs) = True
+startsWithZero (x:xs) = False
+startsWithZero [] = False
+
+sumNumbers :: [Int] -> Int
+sumNumbers [] = 0 
+sumNumbers (x:xs) = x + sumNumbers xs
+
+myMaximum :: [Int] -> Int
+myMaximum [] = 0
+myMaximum (x:xs) = go x xs
+  where go biggest [] = biggest
+        go biggest (x:xs) = go (max biggest x) xs
+
+countNothings :: [Maybe a] -> Int
+countNothings [] = 0 
+countNothings (Nothing : xs) = 1 + countNothings xs
+countNothings (Just _ : xs) = countNothings xs
+
+doableList :: [Int] -> [Int]
+doubleList [] = []
+doubleList (x:xs) = 2*x : doubleList xs
+
+sumNumbers :: [Int] -> Int
+sumNumbers xs = go 0 xs
+  where go sum [] = sum
+        go sum (x:xs) = go (sum+x) xs
+
+doubleList :: [Int] -> [Int]
+doubleList xs = go [] xs
+  where go result [] = result
+        go result (x:xs) = go (result++[2*x]) xs
